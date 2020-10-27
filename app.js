3672bdc552e930aab8049362eee425b4f81bca81
romuald abega <romualdabega@gmail.com>
2020-10-27 20:17:15 +0800

    success button added

--- app.js
+++ app.js
@@ -64,6 +64,9 @@ request.end();
 app.post("/failure", function(req, res){
   res.redirect("/")
 })
+app.post("/success", function(req, res){
+  res.redirect("/")
+})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("the server is responding on port 3000.")
